d17c885f92fcc2f18925e61d0b79cefb1e5b264b
Efi Fogel <efifogel@gmail.com>
2014-03-09 10:12:22 +0200

    v8 example program

---
+++ src/cmds/v8/hello_world.cpp
@@ -0,0 +1,31 @@
+#include <v8.h>
+
+using namespace v8;
+
+int main(int argc, char* argv[]) {
+  // Get the default Isolate created at startup.
+  Isolate* isolate = Isolate::GetCurrent();
+
+  // Create a stack-allocated handle scope.
+  HandleScope handle_scope(isolate);
+
+  // Create a new context.
+  Handle<Context> context = Context::New(isolate);
+
+  // Enter the context for compiling and running the hello world script.
+  Context::Scope context_scope(context);
+
+  // Create a string containing the JavaScript source code.
+  Handle<String> source = String::NewFromUtf8(isolate, "'Hello' + ', World!'");
+
+  // Compile the source code.
+  Handle<Script> script = Script::Compile(source);
+
+  // Run the script to get the result.
+  Handle<Value> result = script->Run();
+
+  // Convert the result to an UTF8 string and print it.
+  String::Utf8Value utf8(result);
+  printf("%s\n", *utf8);
+  return 0;
+}
