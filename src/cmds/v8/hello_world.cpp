548beb3981ffbc1c1a2f23178f61cc49c7e2be71
Efi Fogel <efifogel@gmail.com>
2014-03-09 17:12:22 +0200

    namespace

--- src/cmds/v8/hello_world.cpp
+++ src/cmds/v8/hello_world.cpp
@@ -1,31 +1,30 @@
 #include <v8.h>
 
-using namespace v8;
-
 int main(int argc, char* argv[]) {
   // Get the default Isolate created at startup.
-  Isolate* isolate = Isolate::GetCurrent();
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
 
   // Create a stack-allocated handle scope.
-  HandleScope handle_scope(isolate);
+  v8::HandleScope handle_scope(isolate);
 
   // Create a new context.
-  Handle<Context> context = Context::New(isolate);
+  v8::Handle<v8::Context> context = v8::Context::New(isolate);
 
   // Enter the context for compiling and running the hello world script.
-  Context::Scope context_scope(context);
+  v8::Context::Scope context_scope(context);
 
   // Create a string containing the JavaScript source code.
-  Handle<String> source = String::NewFromUtf8(isolate, "'Hello' + ', World!'");
+  v8::Handle<v8::String> source =
+    v8::String::NewFromUtf8(isolate, "'Hello' + ', World!'");
 
   // Compile the source code.
-  Handle<Script> script = Script::Compile(source);
+  v8::Handle<v8::Script> script = v8::Script::Compile(source);
 
   // Run the script to get the result.
-  Handle<Value> result = script->Run();
+  v8::Handle<v8::Value> result = script->Run();
 
   // Convert the result to an UTF8 string and print it.
-  String::Utf8Value utf8(result);
+  v8::String::Utf8Value utf8(result);
   printf("%s\n", *utf8);
   return 0;
 }
