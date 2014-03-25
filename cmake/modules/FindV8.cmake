bdc662238f9db4debac898c48d677374303a0823
Efi Fogel <efifogel@gmail.com>
2014-03-25 12:34:10 +0200

    Improved build and installation

--- cmake/modules/FindV8.cmake
+++ cmake/modules/FindV8.cmake
@@ -2,9 +2,8 @@
 # Once done this will define
 #  V8_FOUND - System has v8
 #  V8_INCLUDE_DIR - The v8 include directories
-#  V8_LIBRARIES - The libraries needed to use V8
-#  V8_LIB_DIR - The directory where lib files are.
-#               Calling link_directories() with this path is NOT needed.
+#  V8_LIBRARY - The library needed to use V8
+#  V8_LIBRARY_DIR - The directory where lib files are.
 
 find_path(V8_INCLUDE_DIR NAMES v8.h
           PATH_SUFFIXES include
@@ -17,34 +16,45 @@ else()
     set(V8_ARCH x86)
 endif()
 
-if(MSVC_VERSION EQUAL 1600)
-    set(V8_RUNTIME vc10)
-elseif(MSVC_VERSION EQUAL 1700)
-    set(V8_RUNTIME vc11)
-endif()
-
 # CMake>=2.6 supports the notation "debug XXd optimized XX"
-IF(CMAKE_BUILD_TYPE MATCHES Release)
+if (UNIX)
   find_library(V8_LIB_OPT NAMES v8
-             PATH_SUFFIXES lib
-             HINTS ENV V8_LIB_DIR
-	           ENV V8_DIR
-		   ENV V8_DIR/build/Release)
-list(APPEND V8_LIBRARIES optimized ${V8_LIB_OPT})
-else()
-find_library(V8_LIB_DBG NAMES v8d
-             PATH_SUFFIXES lib
-             HINTS ENV V8_LIB_DIR
-	           ENV V8_DIR
-		   ENV V8_DIR/build/Debug)
-list(APPEND V8_LIBRARIES debug ${V8_LIB_DBG})
+               PATH_SUFFIXES lib
+               HINTS ENV V8_LIB_DIR
+	             ENV V8_DIR)
+  get_filename_component(V8_LIBRARY_DIR ${V8_LIB_OPT} PATH)
+  list(APPEND V8_LIBRARY optimized ${V8_LIB_OPT})
+elseif(WIN32)
+  if(MSVC_VERSION EQUAL 1600)
+    set(V8_RUNTIME vc10)
+  elseif(MSVC_VERSION EQUAL 1700)
+    set(V8_RUNTIME vc11)
+  endif()
+
+  IF(CMAKE_BUILD_TYPE MATCHES Release)
+    find_library(V8_LIB_OPT NAMES v8
+                 PATH_SUFFIXES lib
+                 HINTS ENV V8_DIR/build/Release
+		       ENV V8_LIB_DIR
+	               ENV V8_DIR)
+    get_filename_component(V8_LIBRARY_DIR ${V8_LIB_OPT} PATH)
+    list(APPEND V8_LIBRARY optimized ${V8_LIB_OPT})
+  else()
+    find_library(V8_LIB_DBG NAMES v8d
+                 PATH_SUFFIXES lib
+                 HINTS ENV V8_DIR/build/Debug
+		       ENV V8_LIB_DIR
+	               ENV V8_DIR)
+    get_filename_component(V8_LIBRARY_DIR ${V8_LIB_DBG} PATH)
+    list(APPEND V8_LIBRARY debug ${V8_LIB_DBG})
+  endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set Czm_FOUND to TRUE
+# handle the QUIETLY and REQUIRED arguments and set V8_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(V8 DEFAULT_MSG
-                                  V8_LIBRARIES
+                                  V8_LIBRARY
 				  V8_INCLUDE_DIR)
 
-mark_as_advanced(V8_INCLUDE_DIR V8_LIBRARIES)
+mark_as_advanced(V8_INCLUDE_DIR V8_LIBRARY V8_LIBRARY_DIR)
