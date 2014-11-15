b73f4f314c6da9f05e1a7610489682247e159a0b
Efi Fogel <efifogel@gmail.com>
2014-11-16 00:20:50 +0200

    cleaned up

--- src/libs/SCGAL/include/primitives/SCGAL/Exact_number_type.hpp
+++ src/libs/SCGAL/include/primitives/SCGAL/Exact_number_type.hpp
@@ -14,9 +14,6 @@
 // THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A
 // PARTICULAR PURPOSE.
 //
-// $Id: $
-// $Revision: 5325 $
-//
 // Author(s)     : Efi Fogel         <efifogel@gmail.com>
 
 #ifndef SCGAL_EXACT_NUMBER_TYPE_HPP
