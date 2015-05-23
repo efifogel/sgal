044970abfbd8b44249ee61438313f908110bd161
Efi Fogel <efifogel@gmail.com>
2015-05-23 15:45:20 +0300

    Added init_window() & clear_window()

--- src/libs/SGAL/include/utilities/SGAL/Scene.hpp
+++ src/libs/SGAL/include/utilities/SGAL/Scene.hpp
@@ -14,9 +14,6 @@
 // THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A
 // PARTICULAR PURPOSE.
 //
-// $Source$
-// $Revision: 12369 $
-//
 // Author(s)     : Efi Fogel         <efifogel@gmail.com>
 
 #ifndef SGAL_SCEME_HPP
@@ -36,41 +33,85 @@ class Window_item;
 
 class SGAL_SGAL_DECL Scene : public SGAL::Agent {
 public:
-  /*! Destructor */
-  virtual ~Scene() {}
-  
-  /*! Create the scene */
-  virtual void create_scene() {}
+  /*! Destruct.
+   */
+  virtual ~Scene();
 
-  /*! Destroy the scene */
-  virtual void destroy_scene() {}
-  
-  /*! Initialize the scene */  
+  /*! Create the scene.
+   */
+  virtual void create_scene();
+
+  /*! Destroy the scene.
+   */
+  virtual void destroy_scene();
+
+  /*! Initialize the scene.
+   */
   virtual void init_scene() = 0;
 
-  /*! Clear the scene */  
+  /*! Clear the scene.
+   */
   virtual void clear_scene() = 0;
-  
+
+  /*! Initialize a window.
+   * \param[in] window_item the window to initialize
+   */
+  virtual void init_window(Window_item* window_item, Uint width, Uint height);
+
+  /*! Clear a window.
+   * \param[in] window_item the window to initialize
+   */
+  virtual void clear_window(Window_item* window_item);
+
   /*! Reshape the viewport of a window of the scene
    * It is assumed that the window context is the current context
-   * \param window_item the window to reshape
-   * \param width the new width of the window
-   * \param height the new height of the window
+   * \param[in] window_item the window to reshape
+   * \param[in] width the new width of the window
+   * \param[in] height the new height of the window
    */
-  virtual void reshape_window(Window_item* /* window_item */,
-                              Uint /* width */, Uint /* height */) {}
-  
+  virtual void reshape_window(Window_item* window_item, Uint width, Uint height);
+
   /*! Draw into a window of the scene
    * It is assumed that the window context is the current context
-   * \param window_item the window to draw
-   * \param dont_accumulate indicates that no accumulation should be performed
+   * \param[in] window_item the window to draw
+   * \param[in] dont_accumulate indicates whether accumulation not be performed.
    */
-  virtual void draw_window(Window_item* /* window_item */,
-                           Boolean /* dont_accumulate */) {}
+  virtual void draw_window(Window_item* window_item, Boolean dont_accumulate);
 
-  /*! Print the scene information */  
-  virtual void print_scene_help(void) {}
+  /*! Print the scene information.
+   */
+  virtual void print_scene_help(void);
 };
+
+//! \brief destruct.
+inline Scene::~Scene() {}
+
+//! \brief creates the scene.
+inline void Scene::create_scene() {}
+
+//! \brief destroys the scene.
+inline void Scene::destroy_scene() {}
+
+//! \brief initializes a window. Typically used to create a context.
+inline void Scene::init_window(Window_item* /* window_item */,
+                               Uint /* width */, Uint /* height */)
+{}
+
+//! \brief clears a window.
+inline void Scene::clear_window(Window_item* /* window_item */) {}
+
+//! \brief reshapes the viewport of a window of the scene.
+inline void Scene::reshape_window(Window_item* /* window_item */,
+                                  Uint /* width */, Uint /* height */)
+{}
+
+//! \brief draws into a window of the scene
+inline void Scene::draw_window(Window_item* /* window_item */,
+                               Boolean /* dont_accumulate */)
+{}
+
+//! \brief prints the scene information */
+inline void Scene::print_scene_help(void) {}
 
 SGAL_END_NAMESPACE
 
