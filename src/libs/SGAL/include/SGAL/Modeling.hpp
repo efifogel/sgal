fb38742829c23cf055cf508476a8639a2e747889
Efi Fogel <efifogel@gmail.com>
2016-03-01 10:29:24 +0200

    Introduced configuration of Modeling

---
+++ src/libs/SGAL/include/SGAL/Modeling.hpp
@@ -0,0 +1,182 @@
+// Copyright (c) 2016 Israel.
+// All rights reserved.
+//
+// This file is part of SGAL; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the
+// License. See the file LICENSE.LGPL distributed with SGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the
+// software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+// THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Author(s)     : Efi Fogel         <efifogel@gmail.com>
+
+#ifndef SGAL_MODELING_HPP
+#define SGAL_MODELING_HPP
+
+#include <string>
+
+#include "SGAL/basic.hpp"
+#include "SGAL/Types.hpp"
+#include "SGAL/Container.hpp"
+
+SGAL_BEGIN_NAMESPACE
+
+class Container_proto;
+
+class SGAL_SGAL_DECL Modeling : public Container {
+  friend class Indexed_face_set;
+public:
+  enum {
+    FIRST = Container::LAST - 1,
+    TRIANGULATE,
+    REFINE,
+    FAIR
+  };
+
+  /*! Construct. */
+  Modeling(Boolean proto = false);
+
+  /*! Destruct. */
+  virtual ~Modeling() {}
+
+  /*! Construct the prototype. */
+  static Modeling* prototype();
+
+  /*! Clone. */
+  virtual Container* clone();
+
+  /*! Initialize the node prototype. */
+  virtual void init_prototype();
+
+  /*! Delete the node prototype. */
+  virtual void delete_prototype();
+
+  /*! Obtain the node prototype. */
+  virtual Container_proto* get_prototype();
+
+  /// \name field handlers
+  //@{
+  Boolean* triangulate_handle(const Field_info*) { return &m_triangulate; }
+  Boolean* refine_handle(const Field_info*) { return &m_refine; }
+  Boolean* fair_handle(const Field_info*) { return &m_fair; }
+  //@}
+
+  /*! Set the attributes of this node. */
+  virtual void set_attributes(Element* elem);
+
+  /*! Set the flag that indicates whether to triangulate holes.
+   * \param[in] flag indicates whether to triangulate holes.
+   */
+  void set_triangulate(Boolean flag);
+
+  /*! Obtain the flag that indicates whether to triangulate holes.
+   * \return indicates whether the triangulations of holes is desired.
+   */
+  Boolean get_triangulate() const;
+
+  /*! Set the flag that indicates whether to refine the triangulations of holes.
+   * \param[in] flag indicates whether the refinement of the triangulations of
+   *                 holes is desired.
+   */
+  void set_refine(Boolean flag);
+
+  /*! Obtain the flag that indicates whether to refine the triangulations of
+   * holes.
+   * \return true if the refinement of the triangulations of holes is desired,
+   *         and false otherwise.
+   */
+  Boolean get_refine() const;
+
+  /*! Set the flag that indicates whether to fair (smooth) the triangulations
+   * of holes.
+   * \param[in] flag indicates whether the fairing of the triangulations of
+   *                 holes is desired,
+   */
+  void set_fair(Boolean flag);
+
+  /*! Obtain the flag that indicates whether to fair (smooth) the
+   * triangulations of holes.
+   * \return true if the fairing of the triangulations of holes is desired,
+   *         and false otherwise.
+   */
+  Boolean get_fair() const;
+
+  /*! Set defualt values. */
+  void reset(Boolean def_triangulate = s_def_triangulate,
+             Boolean def_refine = s_def_refine,
+             Boolean def_fair = s_def_fair);
+
+protected:
+  /*! Obtain the tag (type) of the container. */
+  virtual const std::string& get_tag() const;
+
+private:
+  /*! The tag that identifies this container type. */
+  static const std::string s_tag;
+
+  /*! The node prototype. */
+  static Container_proto* s_prototype;
+
+  /*! Indicates whether to triangulate a hole thereby filling it. */
+  Boolean m_triangulate;
+
+  /*! Indicates whether to refine the triangulation of a hole by applying
+   * local averaging rules.
+   */
+  Boolean m_refine;
+
+  /*! Indicates whether to smooth the triangulation of a hole to obtain
+   * as-smooth-as-possible shape deformation.
+   */
+  Boolean m_fair;
+
+  // default values
+  static const Boolean s_def_triangulate;
+  static const Boolean s_def_refine;
+  static const Boolean s_def_fair;
+};
+
+//! \brief constructs the prototype.
+inline Modeling* Modeling::prototype() { return new Modeling(true); }
+
+//! \brief clones.
+inline Container* Modeling::clone() { return new Modeling(); }
+
+//! \brief obtains the tag (type) of the container.
+inline const std::string& Modeling::get_tag() const { return s_tag; }
+
+//! \brief sets the flag that indicates whether to triangulate holes.
+inline void Modeling::set_triangulate(Boolean flag) { m_triangulate = flag; }
+
+//! \brief obtains the flag that indicates whether to triangulate holes.
+inline Boolean Modeling::get_triangulate() const { return m_triangulate; }
+
+/*! \brief sets the flag that indicates whether to refine the triangulations of
+ * holes.
+ */
+inline void Modeling::set_refine(Boolean flag) { m_refine = flag; }
+
+/*! \brief obtains the flag that indicates whether to refine the triangulations
+ * of holes.
+ */
+inline Boolean Modeling::get_refine() const { return m_refine; }
+
+/*! \brief sets the flag that indicates whether to fair (smooth) the
+ * triangulations of holes.
+ */
+inline void Modeling::set_fair(Boolean flag) { m_fair = flag; }
+
+/*! \brief obtains the flag that indicates whether to fair (smooth) the
+ * triangulations of holes.
+ */
+inline Boolean Modeling::get_fair() const { return m_fair; }
+
+SGAL_END_NAMESPACE
+
+#endif
---
+++ src/libs/SGAL/lib/Modeling.cpp
@@ -0,0 +1,132 @@
+// Copyright (c) 2016 Israel.
+// All rights reserved.
+//
+// This file is part of SGAL; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; version 2.1 of the
+// License. See the file LICENSE.LGPL distributed with SGAL.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the
+// software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+// THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Author(s)     : Efi Fogel         <efifogel@gmail.com>
+
+#include "SGAL/basic.hpp"
+#include "SGAL/Modeling.hpp"
+#include "SGAL/Container_proto.hpp"
+#include "SGAL/Element.hpp"
+#include "SGAL/Utilities.hpp"
+#include "SGAL/Execution_function.hpp"
+#include "SGAL/Field_infos.hpp"
+
+SGAL_BEGIN_NAMESPACE
+
+const std::string Modeling::s_tag = "Modeling";
+Container_proto* Modeling::s_prototype(nullptr);
+
+// Default values
+const Boolean Modeling::s_def_triangulate(false);
+const Boolean Modeling::s_def_refine(false);
+const Boolean Modeling::s_def_fair(false);
+
+//! \brief constructs.
+Modeling::Modeling(Boolean proto) :
+  Container(proto),
+  m_triangulate(s_def_triangulate),
+  m_refine(s_def_refine),
+  m_fair(s_def_fair)
+{}
+
+//! \brief initializes the node prototype.
+void Modeling::init_prototype()
+{
+  if (s_prototype) return;
+  s_prototype = new Container_proto(Container::get_prototype());
+
+  // Add the object fields to the prototype
+  auto exec_func =
+    static_cast<Execution_function>(&Container::set_rendering_required);
+
+  // triangulate
+  auto triangulate_func =
+    static_cast<Boolean_handle_function>(&Modeling::triangulate_handle);
+  s_prototype->add_field_info(new SF_bool(TRIANGULATE, "triangulate",
+                                          Field_info::RULE_EXPOSED_FIELD,
+                                          triangulate_func, s_def_triangulate,
+                                          exec_func));
+
+  // refine
+  auto refine_func =
+    static_cast<Boolean_handle_function>(&Modeling::refine_handle);
+  s_prototype->add_field_info(new SF_bool(REFINE, "refine",
+                                          Field_info::RULE_EXPOSED_FIELD,
+                                          refine_func, s_def_refine,
+                                          exec_func));
+
+  // fair
+  auto fair_func =
+    static_cast<Boolean_handle_function>(&Modeling::fair_handle);
+  s_prototype->add_field_info(new SF_bool(FAIR, "fair",
+                                          Field_info::RULE_EXPOSED_FIELD,
+                                          fair_func, s_def_fair,
+                                          exec_func));
+}
+
+//! \brief deletes the node prototype.
+void Modeling::delete_prototype()
+{
+  delete s_prototype;
+  s_prototype = nullptr;
+}
+
+//! \brief obtains the node prototype.
+Container_proto* Modeling::get_prototype()
+{
+  if (!s_prototype) Modeling::init_prototype();
+  return s_prototype;
+}
+
+//! \brief sets the attributes of the object.
+void Modeling::set_attributes(Element* elem)
+{
+  Container::set_attributes(elem);
+
+  for (auto ai = elem->str_attrs_begin(); ai != elem->str_attrs_end(); ++ai) {
+    const auto& name = elem->get_name(ai);
+    const auto& value = elem->get_value(ai);
+
+    if (name == "triangulate") {
+      set_triangulate(compare_to_true(value));
+      elem->mark_delete(ai);
+      continue;
+    }
+    if (name == "refine") {
+      set_refine(compare_to_true(value));
+      elem->mark_delete(ai);
+      continue;
+    }
+    if (name == "fair") {
+      set_fair(compare_to_true(value));
+      elem->mark_delete(ai);
+      continue;
+    }
+  }
+
+  // Remove all the marked attributes:
+  elem->delete_marked();
+}
+
+//! \brief sets defualt values.
+void Modeling::reset(Boolean def_triangulate, Boolean def_refine,
+                     Boolean def_fair)
+{
+  m_triangulate = def_triangulate;
+  m_refine = def_refine;
+  m_fair = def_fair;
+}
+SGAL_END_NAMESPACE
