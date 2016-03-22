23f24192dfb653d8eb83949f520e3bfd42e4d299
Efi Fogel <efifogel@gmail.com>
2016-03-22 17:23:45 +0200

    Cleaned up

--- src/libs/SGAL/lib/Split_connected_components_action.cpp
+++ src/libs/SGAL/lib/Split_connected_components_action.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2016 Israel.
+// All rights reserved.
+//
+// This file is part of SGAL; you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as
 // published by the Free Software Foundation; version 2.1 of the
 // License. See the file LICENSE.LGPL distributed with SGAL.
