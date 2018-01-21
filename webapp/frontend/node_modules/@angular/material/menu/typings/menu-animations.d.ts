c2790dfd9662aa9179465a5a6f787f9e4af3ff85
jonathanmeguira <jonathan@herolo.co.il>
2018-01-21 16:47:53 +0200

    pushing missing refs

---
+++ webapp/frontend/node_modules/@angular/material/menu/typings/menu-animations.d.ts
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { AnimationTriggerMetadata } from '@angular/animations';
+/**
+ * Animations used by the mat-menu component.
+ * Animation duration and timing values are based on:
+ * https://material.io/guidelines/components/menus.html#menus-usage
+ */
+export declare const matMenuAnimations: {
+    readonly transformMenu: AnimationTriggerMetadata;
+    readonly fadeInItems: AnimationTriggerMetadata;
+};
+/** @deprecated */
+export declare const fadeInItems: AnimationTriggerMetadata;
+/** @deprecated */
+export declare const transformMenu: AnimationTriggerMetadata;
