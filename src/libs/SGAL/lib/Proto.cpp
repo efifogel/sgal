27e40952d6504182650a8e27cdc6a72c0519f79b
Efi Fogel <efifogel@gmail.com>
2018-03-14 11:38:51 +0200

    Added comments and todo items

--- src/libs/SGAL/lib/Proto.cpp
+++ src/libs/SGAL/lib/Proto.cpp
@@ -14,7 +14,9 @@
 // THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A
 // PARTICULAR PURPOSE.
 //
-// Author(s)     : Efi Fogel         <efifogel@gmail.com>
+// SPDX-License-Identifier: GPL-3.0+
+//
+// Author(s): Efi Fogel         <efifogel@gmail.com>
 
 #include <algorithm>
 
@@ -360,9 +362,19 @@ void Proto::set_attributes(Element* elem)
   Container_array& childs = get_childs();
   childs.resize(proto_childs.size());
 
-  //! \todo Instead of simply cloning, we need to replace the fields in the
-  // source sub-graphs with the formal values of corresponding fields given
-  // in the declaration section.
+  /*! \todo Instead of simply cloning, we need to replace the fields in the
+   * source sub-graphs with the formal values of the corresponding fields given
+   * in the declaration section. These values are stored as the initial values
+   * of field-information records (in the prototype). Make sure to skip the
+   * 'children' field-information record when the list of information records
+   * are processed.
+   * \todo need to call:
+   * 1. cont->add_scene_graph(scene_graph), and
+   * 2. scene_graph->add_container(cont)
+   * where cont is the cloned container, for every cloned container.
+   * It implies, that we need to pass the scene graph to the clone() function
+   * as an argument.
+   */
   std::transform(proto_childs.begin(), proto_childs.end(), childs.begin(),
                  [] (Shared_container cont)
                  { return Shared_container(cont->clone()); });
