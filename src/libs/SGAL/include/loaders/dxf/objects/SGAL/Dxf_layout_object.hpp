1dc74aab3eed8887ca204ee2d16b2fda03c246ea
Efi Fogel <efifogel@gmail.com>
2018-05-31 16:01:49 +0300

    Added layout object

--- src/libs/SGAL/include/loaders/dxf/objects/SGAL/Dxf_layout_object.hpp
+++ src/libs/SGAL/include/loaders/dxf/objects/SGAL/Dxf_layout_object.hpp
@@ -26,6 +26,45 @@ SGAL_BEGIN_NAMESPACE
 
 struct Dxf_layout_object : public Dxf_base_object {
   typedef Dxf_base_object                       Base;
+
+  int16_t m_layout_flags; // Flag (bit-coded) to control the following:
+                        // 1 = Indicates the PSLTSCALE value for this layout
+                        //     when this layout is current
+                        // 2 = Indicates the LIMCHECK value for this layout when
+                        // this layout is current
+  int16_t m_tab_order;  // Tab order. This number is an ordinal indicating this
+                        // layout's ordering in the tab control that is attached
+                        // to the AutoCAD drawing frame window. Note that the
+                        // "Model" tab always appears as the first tab
+                        // regardless of its tab order
+  double m_minimum_limits[2]; // Minimum limits for this layout (defined by
+                        // LIMMIN while this layout is current)
+  double m_maximum_limits[2]; // Maximum limits for this layout (defined by
+                        // LIMMAX while this layout is current):
+  double m_insertion_base_point[3]; // Insertion base point for this layout
+                        // (defined by INSBASE while this layout is current):
+  double m_minimum_extents[3]; // Minimum extents for this layout (defined by
+                        // EXTMIN while this layout is current):
+  double m_maximum_extents[3]; // Maximum extents for this layout (defined by
+                        // EXTMAX while this layout is current):
+  double m_elevation;   // Elevation
+  double m_ucs_origin[3]; // UCS origin
+  double m_ucs_x_axis[3]; // UCS X-axis
+  double m_ucs_y_axis[3]; // UCS Y axis
+  int16_t m_ucs_orthographic_type; // Orthographic type of UCS
+                        // 0 = UCS is not orthographic
+                        // 1 = Top; 2 = Bottom
+                        // 3 = Front; 4 = Back
+                        // 5 = Left; 6 = Right
+  Uint m_viewport;      // ID/handle to the viewport that was last active in this
+                        // layout when the layout was current
+  Uint m_table_record;  // ID/handle of AcDbUCSTableRecord if UCS is a named
+                        // UCS. If not present, then UCS is unnamed
+  Uint m_table_record_base; // ID/handle of AcDbUCSTableRecord of base UCS if
+                        // UCS is orthographic (76 code is non-zero). If not
+                        // present and 76 code is non-zero, then base UCS is
+                        // taken to be WORLD
+  Uint m_shade_plot;    // Shade plot ID
 };
 
 SGAL_END_NAMESPACE
--- src/libs/SGAL/lib/loaders/dxf/objects/Dxf_layout_object.cpp
+++ src/libs/SGAL/lib/loaders/dxf/objects/Dxf_layout_object.cpp
@@ -29,6 +29,36 @@ typedef Dxf_record_wrapper<Dxf_layout_object>  Dxf_layout_object_wrapper;
 template <>
 const std::map<int, Dxf_layout_object_wrapper::Record_member>
 Dxf_layout_object_wrapper::s_record_members = {
+  {70, {&Dxf_layout_object::m_layout_flags, 1, 0}},
+  {71, {&Dxf_layout_object::m_tab_order, 1, 0}},
+  {10, {&Dxf_layout_object::m_minimum_limits, 2, 0}},
+  {20, {&Dxf_layout_object::m_minimum_limits, 2, 1}},
+  {11, {&Dxf_layout_object::m_maximum_limits, 2, 0}},
+  {21, {&Dxf_layout_object::m_maximum_limits, 2, 1}},
+  {12, {&Dxf_layout_object::m_insertion_base_point, 3, 0}},
+  {22, {&Dxf_layout_object::m_insertion_base_point, 3, 1}},
+  {32, {&Dxf_layout_object::m_insertion_base_point, 3, 2}},
+  {14, {&Dxf_layout_object::m_minimum_extents, 3, 0}},
+  {24, {&Dxf_layout_object::m_minimum_extents, 3, 1}},
+  {34, {&Dxf_layout_object::m_minimum_extents, 3, 2}},
+  {15, {&Dxf_layout_object::m_maximum_extents, 3, 0}},
+  {25, {&Dxf_layout_object::m_maximum_extents, 3, 1}},
+  {35, {&Dxf_layout_object::m_maximum_extents, 3, 2}},
+  {146, {&Dxf_layout_object::m_elevation, 1, 0}},
+  {13, {&Dxf_layout_object::m_ucs_origin, 3, 0}},
+  {23, {&Dxf_layout_object::m_ucs_origin, 3, 1}},
+  {33, {&Dxf_layout_object::m_ucs_origin, 3, 2}},
+  {16, {&Dxf_layout_object::m_ucs_x_axis, 3, 0}},
+  {26, {&Dxf_layout_object::m_ucs_x_axis, 3, 1}},
+  {36, {&Dxf_layout_object::m_ucs_x_axis, 3, 2}},
+  {17, {&Dxf_layout_object::m_ucs_y_axis, 3, 0}},
+  {27, {&Dxf_layout_object::m_ucs_y_axis, 3, 1}},
+  {37, {&Dxf_layout_object::m_ucs_y_axis, 3, 2}},
+  {76, {&Dxf_layout_object::m_ucs_orthographic_type, 1, 0}},
+  {331, {&Dxf_layout_object::m_viewport, 1, 0}},
+  {345, {&Dxf_layout_object::m_table_record, 1, 0}},
+  {346, {&Dxf_layout_object::m_table_record_base, 1, 0}},
+  {333, {&Dxf_layout_object::m_shade_plot, 1, 0}}
 };
 
 SGAL_END_NAMESPACE
