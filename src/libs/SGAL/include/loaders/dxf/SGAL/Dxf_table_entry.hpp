fc212044fb536f64862ecdee736b880793a5c3bc
Efi Fogel <efifogel@gmail.com>
2018-06-05 13:13:22 +0300

    Added init(). Changed Uint to String for certain codes.

--- src/libs/SGAL/include/loaders/dxf/SGAL/Dxf_table_entry.hpp
+++ src/libs/SGAL/include/loaders/dxf/SGAL/Dxf_table_entry.hpp
@@ -31,14 +31,17 @@ SGAL_BEGIN_NAMESPACE
 
 struct SGAL_SGAL_DECL Dxf_table_entry {
   String m_handle;      // Handle
-  Uint m_owner_dict;    // Hard owner ID/handle to owner dictionary (optional)
-  Uint m_owner_obj;     // Soft-pointer ID/handle to owner object
+  String m_owner_dict;  // Hard owner ID/handle to owner dictionary (optional)
+  String m_owner_obj;   // Soft-pointer ID/handle to owner object
   std::map<String, std::vector<String> > m_xdata; // "{ACAD_XDICTIONARY" and "}"
                         // indicate the start and end, respectively, of an
                         // extension dictionary group.
                         // This group exists only if persistent reactors have
                         // been attached to this object (optional)
   std::vector<Dxf_extended_data> m_extended_data;
+
+  /*! Initialize */
+  void init() {}
 };
 
 SGAL_END_NAMESPACE
