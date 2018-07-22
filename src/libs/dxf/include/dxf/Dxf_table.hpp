ecdf227e8139e4852334688802fc33eb742d50f0
Efi Fogel <efifogel@gmail.com>
2018-07-22 18:10:30 +0300

    Added find()

--- src/libs/dxf/include/dxf/Dxf_table.hpp
+++ src/libs/dxf/include/dxf/Dxf_table.hpp
@@ -20,6 +20,7 @@
 #define DXF_TABLE_HPP
 
 #include <map>
+#include <vector>
 
 #include <boost/variant.hpp>
 
@@ -39,6 +40,17 @@ struct SGAL_SGAL_DECL Dxf_table : Dxf_base_table {
   Dxf_record_wrapper<Entry> m_entry_wrapper;
   std::vector<Entry> m_entries;
 
+  /*! Find an entry by name.
+   */
+  typename std::vector<Entry>::const_iterator find(const std::string& name)
+  {
+    return std::find_if(m_entries.begin(), m_entries.end(),
+                        [&](const Entry& entry)
+                        { return (entry.m_name == name); });
+  }
+
+  /*! Clear all entries.
+   */
   void clear() { m_entries.clear(); }
 };
 
