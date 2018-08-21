44cd01df0ae98f92dab1e8ed805fb27cb99454da
Efi Fogel <efifogel@gmail.com>
2018-08-21 16:55:20 +0300

    Cleaned up

--- src/libs/SGAL/include/SGAL/Font_style.hpp
+++ src/libs/SGAL/include/SGAL/Font_style.hpp
@@ -14,7 +14,9 @@
 // THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A
 // PARTICULAR PURPOSE.
 //
-// Author(s)     : Efi Fogel         <efifogel@gmail.com>
+// SPDX-License-Identifier: GPL-3.0+
+//
+// Author(s): Efi Fogel         <efifogel@gmail.com>
 
 #ifndef SGAL_FONT_STYLE_HPP
 #define SGAL_FONT_STYLE_HPP
